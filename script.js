98b5dc47e79aa59013922b8a380fac4a1568ef71
Spenser Leighton <spenser.leighton@gmail.com>
2018-03-31 14:35:42 -0600

    Append html to page

--- script.js
+++ script.js
@@ -13,19 +13,6 @@ var ideaCounter = 0;
 //event listeners 
 $saveInputButton.on('click', addIdeaToList);
 
-function addIdeaToList(e) {
-  e.preventDefault();
-ideaCounter++;
-function Idea(title, body, id) {
-  this.title = title,
-  this.body = body,
-  this.id = id
-}
-
-console.log(ideaCounter);
-var coolstuff = new Idea($titleInput.val(), $bodyInput.val()  , ideaCounter)
-console.log(coolstuff);
-}
 
 $('ul').on('click', '.idea article .up-vote', function() {
   if ($(this).hasClass('swill')) {
@@ -42,9 +29,9 @@ $('ul').on('click', '.idea article .up-vote', function() {
 
 $('ul').on('click', '.idea article .down-vote', function() {
   if ($(this).hasClass('genius')) {
-  $(this).removeClass('genius');
-  $(this).addClass('plausible')
-  $('.quality').text('Quality: Plausible');
+  $(this).closest('genius').removeClass();
+  $(this).closest('plausible').remove();
+  $('.quality').closest('Quality: Plausible').text();
   } else {
          ($(this).hasClass('plausible')) 
          $(this).removeClass('plausible');
@@ -53,11 +40,52 @@ $('ul').on('click', '.idea article .down-vote', function() {
   } 
 });
 
+//functions
+function Idea(title, body, id) {
+  this.title = title,
+  this.body = body,
+  this.id = id
+}
+
+function addIdeaToList(e) {
+ideaCounter++
+var newIdea = new Idea($titleInput.val(), $bodyInput.val()  , ideaCounter)
+  e.preventDefault();
+console.log(ideaCounter);
+console.log(newIdea);
+$("ul").append(newIdea.toHtml());
+}
+
+Idea.prototype.toHtml = function(){
+ return (`
+    <li id="${this.id}" class="idea">
+        <article>
+          <h1 class="title">${this.title}</h1>
+          <button class='delete-button'>X</button>
+          <p class="body">${this.body}</p>
+          <button class="up-vote swill">U</button>
+          <button class="down-vote swill">D</button>
+          <p class="quality">Quality: swill</p>
+        </article>
+      </li>
+    `);
+};
+
+
+// $("ul").append(`<li id="${ideaCounter}" class="idea">
+//         <article>
+//           <h1 class="title">${$titleInput.val()}</h1>
+//           <button class='delete-button'>X</button>
+//           <p class="body">${$bodyInput.val()}</p>
+//           <button class="up-vote swill">U</button>
+//           <button class="down-vote swill">D</button>
+//           <p class="quality">Quality: swill</p>
+//         </article>
+//       </li>`)
+
 
 
 // Data Model
-// An Idea has an id, title, a body, and a quality.
-// The id should be a unique identifier.
 // title and body are free-form strings.
 // quality .upshould be one of the follow: “genius”, “plausible”, and “swill.”
 // By default, the idea’s “quality” should default to the lowest setting (i.e. “swill”).
