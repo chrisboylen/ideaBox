4cc865b4e14f57b27cc75ea488549360a56d14c1
Spenser Leighton <spenser.leighton@gmail.com>
2018-03-31 12:49:10 -0600

    Add upvote and downvote function

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var $saveInputButton = $('.save-button');
 var $deleteButton = $('.delete-button');
 var $upVoteButton = $('.up-vote');
 var $downVoteButton = $('.down-vote');
+
+var $quality = $('')
 var ideaCounter = 0;
 //event listeners 
 $saveInputButton.on('click', addIdeaToList);
@@ -26,10 +28,34 @@ var coolstuff = new Idea($titleInput.val(), $bodyInput.val()  , ideaCounter)
 console.log(coolstuff);
 }
 
+$('ul').on('click', '.idea article .up-vote', function() {
+  if ($(this).hasClass('swill')) {
+  $(this).toggleClass('plausible');
+  $('.quality').text('Quality: Plausible');
+  } else {
+         ($(this).hasClass('plausible')) 
+         $(this).toggleClass('genius');
+         $('.quality').text('Quality: Genius');
+  } 
+});
+
+// $('ul').on('click', '.idea article .down-vote', function() {
+//   if ((this).className === 'down-vote') {
+//   $(this).toggleClass('plausible');
+//   $('.quality').text('Quality: Plausible');
+  
+//   } else {
+//   $(this).toggleClass('swill');
+//   $('.quality').text('Quality: swill');
+//     }
+// })
+
+
+
 
 // Data Model
 // An Idea has an id, title, a body, and a quality.
 // The id should be a unique identifier.
 // title and body are free-form strings.
-// quality should be one of the follow: “genius”, “plausible”, and “swill.”
+// quality .upshould be one of the follow: “genius”, “plausible”, and “swill.”
 // By default, the idea’s “quality” should default to the lowest setting (i.e. “swill”).
